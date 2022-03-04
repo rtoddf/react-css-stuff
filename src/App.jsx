708fdbc9a0ee3479362ee2e6eed17d5b08e6b4ec
rtoddf <rtoddf@me.com>
2022-03-04 15:41:15 -0500

    setting up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
 import Keyframes from './Components/Keyframes/default';
 
 function App() {
-  return <>
-    <Keyframes />
-  </>;
+  return <Router>
+    <div className="wrapper">
+      <Switch>
+        <Route exact path='/'>
+          <h3>home page</h3>
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path='/keyframes'>
+          <Keyframes />
+        </Route>
+      </Switch>
+    </div>
+  </Router>;
 }
 
 export default App;
