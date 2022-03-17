c170c08a890afb2569d0706dee55007fc35089b5
rtoddf <rtoddf@me.com>
2022-03-17 12:12:36 -0400

    trying to pass sass vars

--- src/Components/Pocs/NeonType/default.jsx
+++ src/Components/Pocs/NeonType/default.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 import './default.scss';
+import '../../../styles/_colors.scss';
 
 function NeonType() {
-    // const repeatAmt = 6
+    const color = 'color';
+
+    const Frame = styled.div`
+        display: inline-block;
+        margin: 3.75rem;
+        padding: 1.25rem 2.5rem;
+        background-color: $gray-color-3;
+        border: 10px double $blue-green-color;
+        border-radius: 20px;
+        animation: neon-box 1.5s ease-in-out infinite alternate;
+        text-align: center;
+    `;
+
+    const Text = styled.p`
+        margin-bottom: 0;
+        padding: 2.5rem;
+        color: $white-color;
+        font-family: Audiowide;
+        font-size: 7em;
+        text-transform: uppercase;
+        letter-spacing: 2rem;
+        animation: neon-text 1.5s ease-in-out infinite alternate;
+    `;
+
     return (
         <>
             <h3>CSS Neon Type</h3>
             <div className="grid neon-type">
-                <div className="frame">
-                    <p>color</p>
-                </div>
+                <Frame className="frame"><Text>{color}</Text></Frame>
             </div>
         </>
     )
