1e53235faa072e615d32fd48bd767ab22101fb16
rtoddf <rtoddf@me.com>
2022-03-22 16:16:11 -0400

    tweaks and css changes

--- src/Components/Layouts/Panel/default.jsx
+++ src/Components/Layouts/Panel/default.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Image from '../../Common/Image/default';
+import List from './List';
 
 import data from '../../../Data/panels.json';
 import './default.scss';
 
 function Panel() {
     const repeatAmt = 3;
+
     const panels = (data.television).map((item, id) => {
         return (
             <article key={id} className="panel">
 
-                <h3>{item.headline}</h3>
+                <h3>{item.panelTitle}</h3>
 
                 <Image
                     src={item.images.main.src}
@@ -19,7 +21,10 @@ function Panel() {
                     credit={item.images.main.credit}
                 />
 
-                <p>{item.text}</p>           
+                {item.headline !== '' && ( <h4>{item.headline}</h4>  )}
+                {item.text !== '' && ( <div className="body-copy"><p>{item.text}</p></div>  )}
+                {item.list && ( <List listItems={item.list} />  )}
+
             </article>
         );
     });
