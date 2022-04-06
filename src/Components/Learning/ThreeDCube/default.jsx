a11e5ab01d9ad58ebe61e483eba17505f234f657
rtoddf <rtoddf@me.com>
2022-04-06 10:31:39 -0400

    added component code for 3-d cube

--- src/Components/Learning/ThreeDCube/default.jsx
+++ src/Components/Learning/ThreeDCube/default.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
-import Circle from '../../Common/Circle/default';
+
 import './default.scss';
 
 function ThreeDCube() {
-    const repeatAmt = 6
+    const cubeInfo = ["front", "back", "right", "left", "top", "bottom"];
+
+    const cubeSides = cubeInfo.map((side, index) => {
+        return <figure key={index} className={side}>{index + 1}</figure>;
+    });
+
+    const buttons = cubeInfo.map((side, index) => {
+        return <button key={index} className={`show-${side}`}>{`Show ${index + 1}`}</button>;
+    });
+
+    // const box = document.querySelector('#cube');
+    // const panelClassName = 'show-front';
+    // const buttons = document.querySelector('#show-buttons');
+
     return (
         <>
-            <h3>CSS Animations - Pulse</h3>
-            <div className="grid pulse" data-col={repeatAmt}>
-                {[...Array(repeatAmt)].map((el, index) => (
+            <h3>Learning - 3-D Cube</h3>
+            <div className="grid three-d-cube">
+                {/* {[...Array(repeatAmt)].map((el, index) => (
                     <Circle classNames='small' />
-                ))}
+                ))} */}
+
+                <article className="cube-container">
+                    <div id="cube" className="show-front">
+                        {cubeSides}
+                    </div>
+                </article>
+
+                <article className="options">
+                    <p id="show-buttons">
+                        {buttons}
+                    </p>
+
+                    <p>
+                        <button id="toggle-backface-visibility">Toggle Backface Visibility</button>
+                    </p>
+                </article>
             </div>
         </>
     )
