4d27a26f3dc471950174db320fb6681d726fe076
rtoddf <rtoddf@me.com>
2022-04-18 15:40:32 -0400

    cleaning up API calls

--- src/Components/Apis/Itunes/default.js
+++ src/Components/Apis/Itunes/default.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import GetItunesData from '../../../Apis/GetItunesData';
+import { ItunesData } from '../../../Apis/Itunes';
 import './default.scss';
 
 function Itunes() {
+  const artistName = 'Charlie Puth';
+  const songTitle = 'Marvin Gaye';
+  const entity = 'musicTrack';
+
+  const data = ItunesData(artistName, songTitle, entity);
+
+  const images = data.map((image, index) => {
+    if (image.kind !== 'music-video' && image.artistName === artistName) {
+      return (
+        <img
+          key={index}
+          src={image.artworkUrl100.replace('100x100', '225x225')}
+          alt={image.artistName}
+        />
+      );
+    }
+  });
+
   return (
     <div className='itunes'>
-      <GetItunesData />
+      <h3>
+        {artistName} - {songTitle}
+      </h3>
+      <div className='grid' data-col='4'>
+        {images}
+      </div>
     </div>
   );
 }
