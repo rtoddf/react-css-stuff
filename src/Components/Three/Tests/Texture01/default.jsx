c68727971c2dc4d8d084235812b93fe20df94a33
rtoddf <rtoddf@me.com>
2022-05-09 14:42:21 -0400

    trying custom shaders

--- src/Components/Three/Tests/Texture01/default.jsx
+++ src/Components/Three/Tests/Texture01/default.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import * as THREE from 'three';
+import vertexShader from './shaders/vertex.glsl';
+import { MeshBasicMaterial } from 'three';
 import Description from '../../../Common/Description/default';
 import '../../default.scss';
 
@@ -13,7 +15,12 @@ function Texture01() {
         const canvasHeight = canvasWidth * 0.5;
 
         // create a camera
+        // the sphere has a radius of 5
+        // if we don't set the camera's z position, then we're inside the sphere
+        // MeshBasicMaterial doesn't color the inside, so we don't see if
+        // we have to set the z position to more than the radius of the sphere - thus 10
         const camera = new THREE.PerspectiveCamera(75, canvasWidth/canvasHeight, 0.1, 1000);
+        camera.position.z = 10;
         
         // create a renderer
         const renderer = new THREE.WebGLRenderer({
@@ -23,7 +30,19 @@ function Texture01() {
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(canvasWidth, canvasHeight);
 
-        // add FPS stats
+        const sphere = new THREE.Mesh(
+            new THREE.SphereGeometry(5, 50, 50),
+            new THREE.ShaderMaterial({
+                vertexShader,
+                // fragmentShader: 
+            })
+        //     new THREE.MeshBasicMaterial({
+        //         // color: 0xbaba71,
+        //         map: new THREE.TextureLoader().load('/images/threejs/textures/globe.jpeg')
+        //     })
+        );
+
+        scene.add(sphere);
 
         // add animate
         function animate(){
@@ -35,7 +54,7 @@ function Texture01() {
 
     return (
         <>
-            <Description title="Texture One" copy="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor aliquet lacus ut dignissim. In pellentesque lorem eu nisl pretium fermentum. Donec ut tellus imperdiet, vestibulum nisl sed, pellentesque purus. Donec sollicitudin sapien in nibh aliquam pellentesque." />
+            <Description title="Texture One One" copy="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor aliquet lacus ut dignissim. In pellentesque lorem eu nisl pretium fermentum. Donec ut tellus imperdiet, vestibulum nisl sed, pellentesque purus. Donec sollicitudin sapien in nibh aliquam pellentesque." />
             <div className="grid">
                 <div id="shape-holder" className="half">
                     <canvas id="bg"></canvas>
