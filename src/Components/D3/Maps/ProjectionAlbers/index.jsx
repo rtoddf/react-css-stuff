f3366e0f7956a0f6f6fcf19d7d009dd8c10f20f0
rtoddf <rtoddf@me.com>
2022-06-21 13:40:48 -0400

    us map with states done

--- src/Components/D3/Maps/ProjectionAlbers/index.jsx
+++ src/Components/D3/Maps/ProjectionAlbers/index.jsx
@@ -1,13 +1,14 @@
 import {useEffect, useRef} from 'react';
 import * as d3 from 'd3';
+import {geoAlbersUsa, geoPath} from 'd3';
 import * as topojson from "topojson-client";
 
 import {theme} from '../../../../styles/Theme';
 import Grid from '../../../Grid';
 import Description from '../../../Description';
-
-import data from '../../data/maps/us.json';
 import { StyledMap } from './Map.styles';
+
+// https://bl.ocks.org/mbostock/4090848
 
 function ProjectionAlbers() {
     const map = useRef();
@@ -18,57 +19,55 @@ function ProjectionAlbers() {
         const width = container_parent.offsetWidth;
         const height = width * .6;
 
-        // set the colors
-        // const colors = d3.scaleOrdinal([
-        //     theme.d3.charts.colors.orange.one,
-        //     theme.d3.charts.colors.blue.one,
-        //     theme.d3.charts.colors.green.one,
-        //     theme.d3.charts.colors.red.one,
-        //     theme.d3.charts.colors.purple.one]
-        // );
-
         const vis = d3.select(map.current)
             .attr('width', width + margins.left + margins.right)
             .attr('height', height + margins.top + margins.bottom)
 
-        const projection = d3.geoAlbersUsa()
+        const projection = geoAlbersUsa()
             // .scale(width)
             // .translate([ width/2, height/2 ]);
         
-        const path = d3.geoPath()
-            .projection(projection);
+        const path = geoPath()
+            // .projection(projection);
 
         // var path = d3.geoPath();
 
         d3.json('https://d3js.org/us-10m.v1.json')
             .then(function(us, error){
+                if (error) throw error;
+
                 console.log('us: ', us)
-                // console.log('topology: ', topojson.feature(topology, topology.objects.land))
 
-                vis.append("g")
-                    .attr("class", "states")
-                    .selectAll("path")
-                    .data(topojson.feature(us, us.objects.states).features)
-                    .enter().append("path")
-                    .attr("d", path)
-                    .style('fill', 'none')
+                vis.append('g')
+                    .attr('class', 'states')
+                        .selectAll('path')
+                            .data(topojson.feature(us, us.objects.states).features)
+                                .enter().append('path')
+                                    .attr('d', path)
+                                    .attr('fill', theme.d3.charts.colors.green.one)
+                                        .each(function(d) {
+                                            d3.select(this).on('mouseover', user_interaction)
+                                            d3.select(this).on('mouseout', user_interaction)
+                                        })
 
-                    vis.append("path")
-                        .attr("class", "state-borders")
-                        .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+                vis.append('path')
+                    .attr('class', 'state-borders')
+                    .attr('d', path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })))
+                    .attr('fill', 'none')
+                    .attr('stroke', theme.d3.charts.colors.grays.white)
+                    .attr('stroke-width', 0.5)
+                    .style('stroke-linejoin', 'round')
+                    .style('stroke-linecap', 'round')
             });
 
-        // d3.json('https://d3js.org/us-10m.v1.json', function(error, topology){
+            function user_interaction(e, d){
+                // console.log('mouseevent: ', e)
+                // console.log('object: ', d)
 
-        //     
-
-        //     vis.append('path')
-        //         .datum(topojson.feature(topology, topology.objects.land))
-        //             .attr('d', path)
-        //             .attr('fill', '#baba71')
-        //             .attr('stroke', '#333')
-        //             .attr('stroke-width', 0.5)
-        // })        
+                d3.select(this)
+                    .attr('fill', () => e.type === 'mouseover' ? 'orange' : theme.d3.charts.colors.green.one)
+                    .style('cursor', 'pointer')
+            }
             
     });
 
