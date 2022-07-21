d791f3fb6c9d0b6f961896cac7466d2b0ce20257
rtoddf <rtoddf@me.com>
2022-07-21 12:08:27 -0400

    getting rid of files with a 2

--- src/Components/Pages/Apis 2.jsx
+++
@@ -1,8 +0,0 @@
-import SectionContent from '../SectionContent/default';
-import menuData from '../../Data/menu.json';
-
-function Apis() {
-    return <SectionContent section={menuData.apis} />;
-}
-
-export default Apis;
