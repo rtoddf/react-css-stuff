768b5726f2763c3df4e8de1e7750d36f7e7cf2b2
rtoddf <rtoddf@me.com>
2022-09-19 13:21:52 -0400

    set up for swiper trial

--- src/Components/Pocs/Swiper/index.jsx
+++ src/Components/Pocs/Swiper/index.jsx
@@ -1,21 +1,19 @@
 import Grid from '../../Grid';
 import Description from '../../Description';
 import Circle from '../../Reference/Circle';
-import { StyledPulse } from './Pulse.styled';
+import { StyledSwiper } from './Swiper.styled';
 // import './default.scss';
 
 function Swiper() {
     const repeatAmt = 6
     return (
         <>
-            <Description title="CSS Animations - Pulse" copy="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor aliquet lacus ut dignissim. In pellentesque lorem eu nisl pretium fermentum. Donec ut tellus imperdiet, vestibulum nisl sed, pellentesque purus. Donec sollicitudin sapien in nibh aliquam pellentesque." />
-            <StyledPulse>
+            <Description title="Swiper" copy="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor aliquet lacus ut dignissim. In pellentesque lorem eu nisl pretium fermentum. Donec ut tellus imperdiet, vestibulum nisl sed, pellentesque purus. Donec sollicitudin sapien in nibh aliquam pellentesque." />
+            <StyledSwiper>
                 <Grid cols={repeatAmt}>
-                    {[...Array(repeatAmt)].map((el, index) => (
-                        <Circle key={index} classNames='pulse small' />
-                    ))}
+                    <h3>something</h3>
                 </Grid>
-            </StyledPulse>
+            </StyledSwiper>
         </>
     )
 }
